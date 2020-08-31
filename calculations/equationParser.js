563b85c7d40463ac77dcf8a5cc62afcfc91a505c
Avi Fischer <a7fische@uwaterloo.ca>
2020-08-31 16:51:26 -0400

    Worked on input parsing, added error checking

--- calculations/equationParser.js
+++ calculations/equationParser.js
@@ -1,8 +1,17 @@
+class InvalidInputError extends Error{
+    constructor(message){
+        super(message)
+        this.name = "InvalidInputError"
+    }
+}
 
 export default class EquationParser {
 
     static parseSkeletonEquation(input) {
         input = input.replace(/\s/g,'')
+        if(! /[A-Za-z0-9\+]/.test(input)) {
+            throw new InvalidInputError("Invalid Character(s)")
+        }
         var compounds = input.split('+')
         var output = []
         for(var compound of compounds){
@@ -36,6 +45,30 @@ export default class EquationParser {
             }
         }
         return atoms
+    }
+
+    static parseBalancedEquation(input) {
+        input = input.replace(/\s/g,'')
+        if(! /[A-Za-z0-9\+]/.test(input)) {
+            throw new InvalidInputError("Invalid Character(s) in input")
+        }
+        var compounds = input.split('+')
+        var output = []
+        for(var compound of compounds){
+            var number = ''
+            index = 0
+            while(/[0-9]/.test(compound.charAt(index))){
+                index ++
+                number += compound.charAt(0)
+            }
+            if(number == '')
+                number = '1'
+            parsedCompound = EquationParser.parseCompound(compound.substring(index + 1))
+            parsedCompound['amount'] = parseInt(number)
+            output.push[parsedCompound]
+        }
+
+        return output
     }
 
 }
